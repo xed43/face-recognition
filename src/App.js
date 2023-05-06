8587f85850099cc71e468cf987a89ce1ebb6688f
Edward Lopez <edtolo92@gmail.com>
2023-05-06 00:32:48 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
-
+import Rank from "./components/Rank/Rank";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+   console.log('click')
+  };
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
       </div>
     );
   }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="white f3">{"Edward, your current rank is..."}</div>
+      <div className="white f2">{"#5"}</div>
+    </div>
+  );
+};
+
+export default Rank;
